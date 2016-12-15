ca15e46e8469684d0b45ed2e276bc944ed470a71
lirigoyen <irigoyen444@gmail.com>
2016-12-15 04:03:20 -0300

    Bootstrap added
    Bootstrap added

---
+++ main.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Equite</title>
+
+            <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="css/bootstrap.css">
+
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="js/bootstrap.js"></script>
+
+        <style>
+            body {
+                margin: 0;
+                background-color: #CCCCCC;
+            }
+        </style>
+
+    </head>
+    <body>
+        <div class="col-md-12">
+            <p>Hola</p>
+            <button type="button" class="btn btn-default" onclick="window.location='register.php';">Registro</button>
+        </div>
+    </body>
+</html>
---
+++ register.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Equite</title>
+
+            <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="css/bootstrap.css">
+
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="js/bootstrap.js"></script>
+
+    </head>
+    <body>
+        <div class="container">
+            <h1 class="well">Registro</h1>
+        	<div class="col-lg-10 well">
+            	<div class="row">
+                    <form>
+                        <div class="col-sm-10">
+                            <div class="row">
+                                <div class="col-sm-6 form-group">
+                                    <label>Username</label>
+                                    <input type="text" placeholder="Enter First Name Here.." class="form-control">
+                                </div>
+                                <div class="col-sm-6 form-group">
+                                    <label>First Name</label>
+                                    <input type="text" placeholder="Enter First Name Here.." class="form-control">
+                                </div>
+                                <div class="col-sm-6 form-group">
+                                    <label>Last Name</label>
+                                    <input type="text" placeholder="Enter Last Name Here.." class="form-control">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label>Phone Number</label>
+                                <input type="text" placeholder="Enter Phone Number Here.." class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label>Email Address</label>
+                                <input type="text" placeholder="Enter Email Address Here.." class="form-control">
+                            </div>
+                                <div class="form-group">
+                                <label>Website</label>
+                                <input type="text" placeholder="Enter Website Name Here.." class="form-control">
+                            </div>
+                            <button type="button" class="btn btn-lg btn-info">Submit</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
